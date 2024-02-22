dc606f45400e381ac184b245ff77762f0ee48b2e
dieghino26 <brotzadiego@gmail.com>
2024-02-22 01:04:55 +0100

    created table

--- script/index.php
+++ script/index.php
@@ -46,9 +46,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--Bootstrap-->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css' integrity='sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==' crossorigin='anonymous'/>
+
     <title>Hotels</title>
 </head>
 <body>
-   
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Name</th>
+      <th scope="col">Description</th>
+      <th scope="col">Parking</th>
+      <th scope="col">Vote</th>
+      <th scope="col">Distance</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach($hotels as $hotel) : ?>
+        <tr>
+            <?php foreach($hotel as $value) : ?>
+                <td><?=$value?></td>
+            <?php endforeach?>
+        </tr>
+    <?php endforeach?>
+    <tr>
+      
+    </tr>
+  </tbody>
+</table>
 </body>
 </html>
